814a192eaf942d7378ca8d2c566d9f0a4245b53e
Marcelo <marcelo.bojikian@gmail.com>
2021-07-13 16:46:22 +0100

    Fix transaction action

--- src/main/java/com/cashhouse/cashier/controller/TransactionActionController.java
+++ src/main/java/com/cashhouse/cashier/controller/TransactionActionController.java
@@ -1,13 +1,18 @@
 package com.cashhouse.cashier.controller;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import com.cashhouse.cashier.dto.output.TransactionDetailDto;
 import com.cashhouse.cashier.model.Transaction;
 import com.cashhouse.cashier.service.TransactionService;
 
@@ -23,8 +28,13 @@ public class TransactionActionController {
 	@PostMapping("/finish")
 	@ResponseStatus(HttpStatus.ACCEPTED)
 	@ApiOperation(value = "Returns finished transaction entity", response = Transaction.class)
-	public Transaction finish(@PathVariable Long id) {
-		return transactionService.finish(id);
+	public ResponseEntity<TransactionDetailDto> finish(@PathVariable Long id, UriComponentsBuilder uriBuilder) {
+
+		Transaction transaction = transactionService.finish(id);
+
+		URI uri = uriBuilder.path("/api/v1/transactions/{id}").buildAndExpand(transaction.getId()).toUri();
+		return ResponseEntity.created(uri).body(TransactionDetailDto.convert(transaction));
+
 	}
 
 }
